d948c1ca1adebb68b889b9ac3e6c34f94005e5c1
ihor.bondarenko <ihor.bondarenko@slovenskoit.sk>
2024-06-26 00:45:01 +0200

    Fixed Schema for ErrorResponse

--- adapter/rest-api/api-dto/src/main/java/com/di/hostme/service/rest/api/dto/common/error/ErrorResponse.java
+++ adapter/rest-api/api-dto/src/main/java/com/di/hostme/service/rest/api/dto/common/error/ErrorResponse.java
@@ -26,7 +26,7 @@ public interface ErrorResponse {
           """
             [
               {
-                "code": "NOT_FOUND",
+                "code": "COUNTRY_DOES_NOT_EXIST",
                 "message": "Country with id '%s' does not exist.",
                 "messageParameters": [
                   "e9ee4c87-8633-4393-b6a6-1b7778585d51"
