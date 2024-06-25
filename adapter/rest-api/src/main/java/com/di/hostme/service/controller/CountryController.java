ac4a318ecd6419009cf1cd0bb75e3a98ea24883b
ihor.bondarenko <ihor.bondarenko@slovenskoit.sk>
2024-06-25 23:46:57 +0200

    Changed tag description for CountryController

--- adapter/rest-api/src/main/java/com/di/hostme/service/controller/CountryController.java
+++ adapter/rest-api/src/main/java/com/di/hostme/service/controller/CountryController.java
@@ -21,7 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Countries", description = "Countries endpoints")
+@Tag(name = "Countries", description = "Country endpoints")
 @RestController
 public class CountryController extends BaseController {
 
